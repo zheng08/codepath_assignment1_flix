b5bddbe2eb136b3f85178956dd646f6b60225133
zheng08 <zheng08@seas.upenn.edu>
2022-09-13 17:57:21 -0700

    Added assignment 1 files to repo

---
+++ MovieApp/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by Yu Zheng on 9/13/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MovieApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MovieApp
+  pod "AlamofireImage"
+
+  target 'MovieAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MovieAppUITests' do
+    # Pods for testing
+  end
+
+end
